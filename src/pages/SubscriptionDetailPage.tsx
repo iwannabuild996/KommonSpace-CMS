f82d11512653a43ca3a49f91122bcd68a1292948
Shahin Abdulla <shahinabdullamp@gmail.com>
2025-12-18 15:45:57 +0530

    Company

--- src/pages/SubscriptionDetailPage.tsx
+++ src/pages/SubscriptionDetailPage.tsx
@@ -949,11 +949,9 @@ export default function SubscriptionDetailPage() {
                                             <svg className="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
                                             </svg>
-                                            <div className="ml-4 flex min-w-0 flex-1 gap-2 flex-col items-start">
-                                                <div className="flex gap-2">
-                                                    <span className="truncate font-medium">{file.file_name}</span>
-                                                    <span className="flex-shrink-0 text-gray-400">({file.label})</span>
-                                                </div>
+                                            <div className="ml-4 flex min-w-0 flex-1 flex-col gap-1">
+                                                <span className="truncate font-medium text-gray-900">{file.file_name}</span>
+                                                <span className="text-xs text-gray-500">({file.label})</span>
                                             </div>
                                         </div>
                                         <div className="ml-4 flex-shrink-0">
